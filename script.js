ebfa58cc979037213e01604c7cca9eb14e0660a1
smed12 <76533255+smed12@users.noreply.github.com>
2021-02-26 11:28:33 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+const searchClient = algoliasearch('YOUR APPLICATION ID GOES HERE', 'YOUR SEARCH ONLY API KEY GOES HERE');
+
+const search = instantsearch({
+indexName: 'Email-Index',
+searchClient,
+});
+
+search.addWidgets([
+instantsearch.widgets.searchBox({
+container: '#searchbox',
+}),
+
+instantsearch.widgets.configure({
+hitsPerPage: 16
+}),
+
+instantsearch.widgets.hits({
+container: '#hits',
+templates: {
+item: `
+<div>
+<div class="hit-name">
+{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}
+</div>
+</div>
+`,
+},
+}),
+
+instantsearch.widgets.pagination({
+container: '#pagination',
+}),
+]);
+
+search.start();
